105ad274721e38be8a9f85d4a59987d26f303712
FuG1 <85907308+FuG1@users.noreply.github.com>
2024-09-27 23:17:32 +0300

    change frontend

--- frontend/scripts/global.js
+++
@@ -1,8 +0,0 @@
-// Функция для плавного перехода к секции при клике на пункт меню
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const target = this.getAttribute('href').substring(1);
-        document.getElementById(target)?.scrollIntoView({ behavior: 'smooth' });
-    });
-});
